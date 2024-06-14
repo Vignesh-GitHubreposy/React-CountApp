64fb6f0a609ab65222878a815bc23bc0eeff2827
Vignesh-GitHubreposy <vignesh.pojo@gmail.com>
2024-06-14 16:04:07 +0530

    Added use effect

--- count-app/src/App.js
+++ count-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [auto, setAuto] = useState(0);
   const [count, setCount] = useState(0);
   const handlePlus = () => {
     setCount(count + 1);
@@ -12,19 +13,23 @@ function App() {
       setCount(count - 1);
     }
   };
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setCount(count + 1);
-  //   }, 3000);
-  // }),[];
+  useEffect(() => {
+    setTimeout(() => {
+       setAuto(auto + 1);
+    }, 1000);
+  });
 
   return (
     <div className="App">
+    <div className="App-header">
     <h1>React Count App</h1>
       <p>Press [+] Plus or [-] Minus button to check </p>
       <h1>{count}</h1>
       <button onClick={handlePlus}>[+]</button>
       <button onClick={handleMinus}>[-]</button>
+      <p>Auto Count:{auto}</p>
+    </div>
+    
     </div>
   );
 }
